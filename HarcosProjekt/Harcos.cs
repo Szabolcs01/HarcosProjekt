acb77013ba98c7094ce39ee0f10afe4c4afda701
Szabolcs01 <oszabi000@gmail.com>
2020-10-01 19:44:18 +0200

    Fel tőltöttem adatokkal

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -8,5 +8,21 @@ namespace HarcosProjekt
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.Nev = nev;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
     }
 }
