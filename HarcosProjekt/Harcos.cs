19d5defade61321a7156c66a11c088341e04b57f
Szabolcs01 <oszabi000@gmail.com>
2020-10-01 19:49:24 +0200

    Getter,setter és konstruktor létre hozzása

--- HarcosProjekt/Harcos.cs
+++ HarcosProjekt/Harcos.cs
@@ -15,14 +15,40 @@ namespace HarcosProjekt
         private int alapEletero;
         private int alapSebzes;
 
-        public Harcos(string nev, int statuszSablon)
-        {
-            this.Nev = nev;
-        }
 
         public string Nev { get => nev; set => nev = value; }
         public int Szint { get => szint; set => szint = value; }
         public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
         public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero; set => alapEletero = value; }
+        public int AlapSebzes { get => alapSebzes; set => alapSebzes = value; }
+        public int Sebzes { get => alapSebzes + szint; }
+        public int SzintLepeshez { get => 10 + szint * 5; }
+        public int MaxEletero { get => alapEletero + szint * 3; }
+
+
+        public Harcos(string nev, int statuszSablon)
+        {
+            this.nev = nev;
+            this.szint = 1;
+            this.tapasztalat = 0;
+            if (statuszSablon == 1)
+            {
+                this.alapEletero = 15;
+                this.alapSebzes = 3;
+            }
+            else if (statuszSablon == 2)
+            {
+                this.alapEletero = 12;
+                this.alapSebzes = 4;
+            }
+            else if (statuszSablon == 3)
+            {
+                this.alapEletero = 8;
+                this.alapSebzes = 5;
+            }
+            this.eletero = alapEletero;
+        }
+
     }
 }
